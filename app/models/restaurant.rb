aed841ae6b484c3849070941611daae6625d2aed
Jan Schuetz <janalexschuetz@gmail.com>
2022-11-15 12:12:59 +1100

    Implemented restaurant and reviews modell

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = %w[chinese italian japanese french belgian]
+
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: CATEGORY
+end
